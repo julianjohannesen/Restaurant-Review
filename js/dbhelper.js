f21b2068ebe80cfb1a0664936bc69b55232a4261
Julian Johannesen <julian.johannesen@gmail.com>
2018-09-12 22:49:55 -0400

    url tweak

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -145,14 +145,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`/restaurant.html?id=${restaurant.id}`);
+    return (`https://julianjohannesen.github.io/Restaurant-Review/restaurant.html?id=${restaurant.id}`);
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://julianjohannesen.github.io/Restaurant-Review/img/${restaurant.photograph}`);
   }
 
   /**
